ae4317e7e5ab7cbe5a1af983ccc5128a29b027b8
ardisya <davaas2022@gmail.com>
2022-02-14 20:37:44 +0700

    k

--- src/data/charities.js
+++ src/data/charities.js
@@ -5,17 +5,11 @@ module.exports = {
             title: 'First Charity Project',
             category: 'Money',
             thumbnail: 'assets/images/causes/money.jpg',
-            pledged: 99000,
+            pledged: 0,
             target : 100000,
-<<<<<<< HEAD
-            date_end: +new Date('10 August 2022'),
-            profile_photo: 'assets/images/avatar/cat.jpg', 
-            profile_name: 'Denventa',
-=======
             date_end: +new Date('10 March 2022'),
             profile_photo: 'assets/images/avatar/pp.jpg', 
-            profile_name: 'ul ul ',
->>>>>>> 760f787bf414e6c16069cb515ea33fad0a0cdc5a
+            profile_name: 'Chandra Ferdhiyan',
             no_pledged: 0
         }
     ]
--- src/function/hello.js
+++ src/function/hello.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
     callback(null, {
         statusCode: 200,
-        body: "hello, lambda function!"
+        body: "hello, lambda functions!"
     })
-} ;
+}
